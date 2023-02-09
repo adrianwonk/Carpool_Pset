f8f2fa728238d75e76060b8fc5598f8b6b85bb2b
adrianwong <shotgunfunfun8@gmail.com>
2023-02-09 20:40:37 +0800

    Initial commit

---
+++ app/src/main/java/awong/carpool_pset/MainActivity.java
@@ -0,0 +1,14 @@
+package awong.carpool_pset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
