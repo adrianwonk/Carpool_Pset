86baf36ea394529b0f05e2b217cfb2df22b9c2f7
adrianwong <shotgunfunfun8@gmail.com>
2023-02-21 06:39:53 +0800

    nice

--- app/src/main/java/awong/carpool_pset/Controllers/HomeActivity.java
+++ app/src/main/java/awong/carpool_pset/Controllers/HomeActivity.java
@@ -1,14 +1,16 @@
-package awong.carpool_pset;
+package awong.carpool_pset.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import awong.carpool_pset.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
